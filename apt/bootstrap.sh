d71f241a5dfd6e24eedec2d99343cd1b13f681ba
puhitaku <puhitaku@gmail.com>
2019-11-28 16:44:34 +0900

    Fix bootstrap.sh

--- apt/bootstrap.sh
+++ apt/bootstrap.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-python -m venv env
+python3.7 -m venv env
 . ./env/bin/activate
 
 pip install -r requirements_bootstrap.txt
 
-echo "Run: source ./env/bin/activate && python -m ligath-apt"
+echo "Run: source ./env/bin/activate && python3.7 -m apt"
 

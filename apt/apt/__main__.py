7079d9975275ff6191cd07b66aa498fda42b0486
puhitaku <puhitaku@gmail.com>
2019-12-11 17:12:34 +0900

    apt: make Python 3.5 compatible

--- apt/apt/__main__.py
+++ apt/apt/__main__.py
@@ -1,53 +1,91 @@
-from __future__ import annotations
-
+import json
 import pathlib
 import subprocess
-from dataclasses import dataclass
 from datetime import datetime
-from os import PathLike
 from socket import gethostname
-from typing import Dict, List, Optional, Set, Tuple
+from typing import List, Tuple
 
-from dataclasses_json import dataclass_json
 from debian import copyright as cr
 
 
-@dataclass_json
-@dataclass
 class License:
-    name: str
-    machine_readable_name: str
-    body: str
+    name = ""
+    machine_readable_name = ""
+    body = ""
+
+    def __init__(self, name="", machine_readable_name="", body=""):
+        self.name = name
+        self.machine_readable_name = machine_readable_name
+        self.body = body
+
+    def asdict(self):
+        return {
+            "name": self.name,
+            "machine_readable_name": self.machine_readable_name,
+            "body": self.body,
+        }
 
 
-@dataclass_json
-@dataclass
 class Copyright:
-    copyright: str
-    file_range: List[str]
-    license: License
+    copyright = ""
+    file_range = None
+    license = None
+
+    def __init__(self, copyright="", file_range=None, lic=None):
+        self.copyright = copyright
+        self.file_range = [] if file_range is None else file_range
+        self.license = License() if lic is None else lic
+
+    def asdict(self):
+        return {
+            "copyright": self.copyright,
+            "file_range": self.file_range,
+            "license": self.license.asdict(),
+        }
 
 
-@dataclass_json
-@dataclass
 class Package:
-    name: str
-    version: str
-    copyrights: List[Copyright]
-    raw_copyright: str
+    name = ""
+    version = ""
+    copyrights = None
+    raw_copyright = ""
+
+    def __init__(self, name="", version="", copyrights=None, raw_copyright=""):
+        self.name = name
+        self.version = version
+        self.copyrights = [] if copyrights is None else copyrights
+        self.raw_copyright = raw_copyright
+
+    def asdict(self):
+        return {
+            "name": self.name,
+            "version": self.version,
+            "copyrights": [p.asdict() for p in self.copyrights],
+            "raw_copyright": self.raw_copyright,
+        }
 
 
-@dataclass_json
-@dataclass
 class Output:
-    host: str
-    parsed: List[Package]
-    not_parsed: List[Package]
+    host = ""
+    parsed = None
+    not_parsed = None
+
+    def __init__(self, host="", parsed=None, not_parsed=None):
+        self.host = host
+        self.parsed = [] if parsed is None else parsed
+        self.not_parsed = [] if not_parsed is None else not_parsed
+
+    def asdict(self):
+        return {
+            "host": self.host,
+            "parsed": [p.asdict() for p in self.parsed],
+            "not_parsed": [np.asdict() for np in self.not_parsed],
+        }
 
 
 def parse_copyright(
     package: str,
-    path: PathLike,
+    path: pathlib.Path,
     version: str,
     parsed: List[Package],
     not_parsed: List[Package],
@@ -55,7 +93,7 @@ def parse_copyright(
     raw_copyright = ""
 
     try:
-        with open(path) as f:
+        with open(str(path), encoding="utf-8") as f:
             raw_copyright = f.read()
             f.seek(0)
             c = cr.Copyright(f, strict=False)
@@ -67,7 +105,7 @@ def parse_copyright(
             # will be raised in debian.deb822:1149
             not_parsed.append(Package(package, version, [], raw_copyright))
             return
-        raise RuntimeError(f"unknown ValueError: {e}")
+        raise RuntimeError("unknown ValueError: {}".format(e))
 
     file_par = [p for p in c.all_paragraphs() if isinstance(p, cr.FilesParagraph)]
     lice_par = [p for p in c.all_paragraphs() if isinstance(p, cr.LicenseParagraph)]
@@ -80,7 +118,7 @@ def parse_copyright(
             syn = lp.license.synopsis
         except cr.MachineReadableFormatError as e:
             not_parsed.append(Package(package, version, [], raw_copyright))
-            print(f"{package}: {e}")
+            print("{}: {}".format(package, e))
             return
 
         if syn not in licenses:
@@ -92,16 +130,16 @@ def parse_copyright(
             syn = fp.license.synopsis
         except cr.MachineReadableFormatError as e:
             not_parsed.append(Package(package, version, [], raw_copyright))
-            print(f"{package}: {e}")
+            print("{}: {}".format(package, e))
             return
         except AttributeError as e:
             not_parsed.append(Package(package, version, [], raw_copyright))
-            print(f"{package}: {e}")
+            print("{}: {}".format(package, e))
             return
 
         if syn is None:
             not_parsed.append(Package(package, version, [], raw_copyright))
-            print(f"Unknown License!!: {fp.license.synopsis}")
+            print("Unknown License!! {}".format(fp.license.synopsis))
             return
 
         lic = licenses.get(syn, None)
@@ -115,37 +153,39 @@ def parse_copyright(
 
 
 def gather_apt_licenses():
-    out: bytes = subprocess.check_output("dpkg -l | grep '^ii'", shell=True)
+    out = subprocess.check_output("dpkg -l | grep '^ii'", shell=True)
 
-    packages: List[Tuple[str, str]] = []
+    packages = []
     for l in out.decode().split("\n")[:-1]:
         tokens = [s for s in l.split(" ") if s != ""]
         packages.append((tokens[1].split(":")[0], tokens[2]))
 
-    exist: List[Tuple[str, str, PathLike]] = []
-    not_exist: List[Tuple[str, str]] = []
+    exist = []
+    not_exist = []
 
     for p, v in packages:
-        path = pathlib.Path(f"/usr/share/doc/{p}/copyright")
+        path = pathlib.Path("/usr/share/doc/{}/copyright".format(p))
         if path.exists():
             exist.append((p, v, path))
         else:
-            print(f"{p}: copyright file was not found")
+            print("{}: copyright file was not found".format(p))
             not_exist.append((p, v))
 
-    parsed: List[Package] = []
-    not_parsed: List[Package] = [Package(p, v, [], "") for p, v in not_exist]
+    parsed = []
+    not_parsed = [Package(p, v, [], "") for p, v in not_exist]
 
     for package, version, path in exist:
         parse_copyright(package, path, version, parsed, not_parsed)
 
-    print(f"Summary: parsed({len(parsed)}), parse error({len(not_parsed)})")
+    print("Summary: parsed({}), parse error({})".format(len(parsed), len(not_parsed)))
 
-    out: str = Output(gethostname(), parsed, not_parsed).to_json(
-        ensure_ascii=False, indent=" "
-    )
-    with open(datetime.now().strftime("%y%m%d") + ".json", "w") as f:
-        f.write(out)
+    with open(datetime.now().strftime("%y%m%d") + ".json", "w", encoding="utf-8") as f:
+        json.dump(
+            Output(gethostname(), parsed, not_parsed).asdict(),
+            f,
+            ensure_ascii=False,
+            indent=" ",
+        )
 
 
 gather_apt_licenses()
--- apt/bootstrap.sh
+++ apt/bootstrap.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-python3.7 -m venv env
+python3 -m venv env
 . ./env/bin/activate
 
 pip install -r requirements_bootstrap.txt
 
-echo "Run: source ./env/bin/activate && python3.7 -m apt"
+echo "Run: source ./env/bin/activate && python3 -m apt"
 

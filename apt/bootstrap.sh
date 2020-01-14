323f0801ecce0fee1182a3335bb086eacd7abe22
puhitaku <puhitaku@gmail.com>
2020-01-14 17:31:23 +0900

    Launch pip3 explicitly

--- apt/bootstrap.sh
+++ apt/bootstrap.sh
@@ -3,7 +3,7 @@
 python3 -m venv env
 . ./env/bin/activate
 
-pip install -r requirements_bootstrap.txt
+pip3 install -r requirements_bootstrap.txt
 
 echo "Run: source ./env/bin/activate && python3 -m apt"
 

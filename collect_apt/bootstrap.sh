6d23c852c23f5900e8fe44cb2334bd1deb14641f
puhitaku <puhitaku@gmail.com>
2020-01-16 15:37:38 +0900

    Rename apt -> collect_apt

--- collect_apt/bootstrap.sh
+++ collect_apt/bootstrap.sh
@@ -5,5 +5,5 @@ python3 -m venv env
 
 pip3 install -r requirements_bootstrap.txt
 
-echo "Run: source ./env/bin/activate && python3 -m apt"
+echo "Run: source ./env/bin/activate && python3 -m collect_apt"
 
